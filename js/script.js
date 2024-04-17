4a62a253b2df244f1060f66f925831ff176a66b2
Emilio Gallo <emilio.gallo17@gmail.com>
2024-04-17 18:11:37 +0200

    add: greetings&img - html&js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue;
+
+createApp({
+
+   data() {
+
+      return {
+
+         message: "Hello",
+         imgDescription: "image description",
+         imgURL: "https://picsum.photos/200",
+      };
+
+   },
+
+   methods: {
+
+      
+
+   },
+
+}).mount("#container-vue");
